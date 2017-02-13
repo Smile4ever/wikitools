0121fb672443f84f8347226de040ae53d0756d28
Smile4ever <computergeoffrey@zoho.com>
2017-02-13 19:38:24 +0100

    nocat.sh: only urlencode ampersand

--- nocat/nocat.sh
+++ nocat/nocat.sh
@@ -35,21 +35,8 @@ USERPASS=$(cat ${CONFIGPASSWORD} | base64 --decode)
 cookie_jar="data/wikicj" #Will store file in wikifile
 
 rawurlencode() {
-  local string="${1}"
-  local strlen=${#string}
-  local encoded=""
-  local pos c o
-
-  for (( pos=0 ; pos<strlen ; pos++ )); do
-     c=${string:$pos:1}
-     case "$c" in
-        [-_.~a-zA-Z0-9] ) o="${c}" ;;
-        * )               printf -v o '%%%02x' "'$c"
-     esac
-     encoded+="${o}"
-  done
-  echo "${encoded}"    # You can either set a return variable (FASTER) 
-  REPLY="${encoded}"   #+or echo the result (EASIER)... or both... :p
+	ENCODED="${1//\&/\%26}" #replace ampersand by %26
+	echo "${ENCODED}"
 }
 
 while true
